f9097c0141d4a17b99ef6530c29ef043c48e2639
lee teukki <leeteukki@leeui-MacBook-Pro.local>
2021-07-25 16:26:20 +0900

    make SwiftUi

---
+++ fundamental_Tutorial/fundamental Tutorial/fundamental Tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fundamental Tutorial
+//
+//  Created by lee teukki on 2021/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
